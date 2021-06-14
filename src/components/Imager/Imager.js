81caba8ff1ebb8a45c1c6db3b9bd48336b6357ca
dustnhgstrm <77700279+dustnhgstrm@users.noreply.github.com>
2021-06-14 15:26:44 -0400

    handled the remaining logic to make a click on a thumbnail appear in display. also did some css

--- src/components/Imager/Imager.js
+++ src/components/Imager/Imager.js
@@ -26,19 +26,14 @@ export class Imager extends Component {
   };
 
   handleRandomPicture = () => {
-    console.log("click");
     const myRando = Math.floor(Math.random() * 12);
-    console.log(myRando);
     let randomPic = this.state.imageArray[myRando];
     this.setState({
       currentImage: randomPic,
       currentIndex: myRando,
     });
-
-    //generate random number 0-11 and apply that number to the image at that index of image array.
   };
   handleForwardPicture = () => {
-    console.log("click");
     let newIndex = this.state.currentIndex + 1;
     if (newIndex > 11) {
       newIndex = 0;
@@ -48,12 +43,8 @@ export class Imager extends Component {
       currentImage: nextPic,
       currentIndex: newIndex,
     });
-
-    //add one to the index of current image in display and then display photo corresponding to that index
   };
   handleBackwardPicture = () => {
-    console.log("click");
-    console.log("click");
     let newIndex = this.state.currentIndex - 1;
     if (newIndex < 0) {
       newIndex = 11;
@@ -63,7 +54,15 @@ export class Imager extends Component {
       currentImage: nextPic,
       currentIndex: newIndex,
     });
-    //subtract one from the index of the current image in display and then display photo corresponding to that index
+  };
+
+  handlesClickOnThumbnail = (item, key) => {
+    let newIndex = key;
+    let newPic = item;
+    this.setState({
+      currentImage: newPic,
+      currentIndex: newIndex,
+    });
   };
 
   render() {
@@ -80,16 +79,25 @@ export class Imager extends Component {
           <button onClick={this.handleRandomPicture} id="random">
             Random
           </button>
-          <button onClick={this.handleBackwardPicture} id="left">
-            &lt;
-          </button>
-          <button onClick={this.handleForwardPicture} id="right">
-            &gt;
-          </button>
+          <div className="left-or-right">
+            <button onClick={this.handleBackwardPicture} id="left">
+              &lt;
+            </button>
+            <button onClick={this.handleForwardPicture} id="right">
+              &gt;
+            </button>
+          </div>
         </div>
         <ul className="grid">
           {this.state.imageArray.map((item, index) => {
-            return <Thumbnail item={item} key={index} />;
+            return (
+              <Thumbnail
+                item={item}
+                index={index}
+                key={index}
+                handlesClickOnThumbnail={this.handlesClickOnThumbnail}
+              />
+            );
           })}
         </ul>
       </div>
--- src/components/Thumbnail/Thumbnail.js
+++ src/components/Thumbnail/Thumbnail.js
@@ -2,15 +2,27 @@ import React, { Component } from "react";
 import "./Thumbnail.css";
 
 export class Thumbnail extends Component {
+  state = {
+    thisPic: "",
+  };
+
+  // handlesClickOnThumbnail = () => {
+  //   console.log("click");
+  //   let thumbnailImage = this.props.item;
+  //   this.props.handlePutThumbnailPictureInDisplay(thumbnailImage);
+  // };
+
   render() {
-    const { item } = this.props;
+    const { item, handlesClickOnThumbnail, index } = this.props;
     return (
       <div className="display-class">
         <li>
           <img
             src={process.env.PUBLIC_URL + `images/${item}`}
             alt="fish"
-            onClick={() => {}}
+            onClick={() => {
+              handlesClickOnThumbnail(item, index);
+            }}
           />
         </li>
       </div>
